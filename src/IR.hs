2c283527e5b675aa4cd2ffa8c29e8cb9e738a462
martyall <martyall@protonmail.com>
2023-12-10 12:38:08 -0800

    added lambdas

---
+++ src/IR.hs
@@ -0,0 +1,139 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use gets" #-}
+
+module IR
+  ( Exp (..),
+    toCoreExpr,
+    applyLambdas,
+    exp_binop,
+    exp_seq,
+  )
+where
+
+import Common (LambdaVar, Op, UnOp, Var, is_assoc)
+import Control.Monad.Error.Class (throwError)
+import Control.Monad.State (State, evalState, get, modify)
+import Data.Kind (Type)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
+import qualified Expr as Core
+import Field (Field)
+
+data Exp :: Type -> Type where
+  EVar :: Var -> Exp a
+  ELambdaVar :: LambdaVar -> Exp a
+  EVal :: (Field a) => a -> Exp a
+  EUnop :: UnOp -> Exp a -> Exp a
+  EBinop :: Op -> [Exp a] -> Exp a
+  EIf :: Exp a -> Exp a -> Exp a -> Exp a
+  EAssert :: Exp a -> Exp a -> Exp a
+  ESeq :: [Exp a] -> Exp a
+  EUnit :: Exp a
+  EAbs :: LambdaVar -> Exp a -> Exp a
+  EApp :: Exp a -> Exp a -> Exp a
+
+deriving instance (Show a) => Show (Exp a)
+
+type Env a = Map LambdaVar (Exp a)
+
+applyLambdas ::
+  Exp a ->
+  Exp a
+applyLambdas expression = evalState (go expression) Map.empty
+  where
+    go :: Exp a -> State (Env a) (Exp a)
+    go = \case
+      e@(EVar _) -> pure e
+      ELambdaVar var -> fromMaybe (ELambdaVar var) . Map.lookup var <$> get
+      e@(EVal _) -> pure e
+      EUnit -> pure EUnit
+      EUnop op e -> EUnop op <$> go e
+      EBinop op es -> EBinop op <$> mapM go es
+      EIf b e1 e2 -> EIf <$> go b <*> go e1 <*> go e2
+      EAssert e1 e2 -> EAssert <$> go e1 <*> go e2
+      ESeq es -> ESeq <$> mapM go es
+      EAbs var e -> EAbs var <$> go e
+      EApp (EAbs var e1) e2 -> do
+        _e2 <- go e2
+        modify (Map.insert var _e2)
+        go e1
+      EApp e1 e2 -> do
+        _e1 <- go e1
+        _e2 <- go e2
+        go (EApp _e1 _e2)
+
+exp_binop :: Op -> Exp a -> Exp a -> Exp a
+exp_binop op e1 e2 =
+  case (e1, e2) of
+    (EBinop op1 l1, EBinop op2 l2)
+      | op1 == op2 && op2 == op && is_assoc op ->
+          EBinop op (l1 ++ l2)
+    (EBinop op1 l1, _)
+      | op1 == op && is_assoc op ->
+          EBinop op (l1 ++ [e2])
+    (_, EBinop op2 l2)
+      | op2 == op && is_assoc op ->
+          EBinop op (e1 : l2)
+    (_, _) -> EBinop op [e1, e2]
+
+-- | Smart constructor for sequence, ensuring all expressions are
+--  flattened to top level.
+exp_seq :: Exp a -> Exp a -> Exp a
+exp_seq e1 e2 =
+  case (e1, e2) of
+    (ESeq l1, ESeq l2) -> ESeq (l1 ++ l2)
+    (ESeq l1, _) -> ESeq (l1 ++ [e2])
+    (_, ESeq l2) -> ESeq (e1 : l2)
+    (_, _) -> ESeq [e1, e2]
+
+toCoreExpr :: (Show a) => Exp a -> Core.Exp a
+toCoreExpr = either error id . toCoreExpr'
+
+toCoreExpr' :: (Show a) => Exp a -> Either String (Core.Exp a)
+toCoreExpr' = \case
+  EVar var -> pure $ Core.EVar var
+  EVal v -> pure $ Core.EVal v
+  EUnit -> pure Core.EUnit
+  EUnop op e -> Core.EUnop op <$> toCoreExpr' e
+  EBinop op es -> Core.EBinop op <$> mapM toCoreExpr' es
+  EIf b e1 e2 -> Core.EIf <$> toCoreExpr' b <*> toCoreExpr' e1 <*> toCoreExpr' e2
+  EAssert e1 e2 -> Core.EAssert <$> toCoreExpr' e1 <*> toCoreExpr' e2
+  ESeq es -> Core.ESeq <$> mapM toCoreExpr' es
+  e -> throwError ("Impossible after IR simplicifaction: " <> show e)
+
+{-
+
+(\x y -> x y) (\x -> x)
+
+aL (EApp (EAbs x (EAbs y (EApp x y))) (Abs z z))
+
+[(x, Abs z z)]
+aL (EAbs y (EApp (EAbs z z) y))
+
+[(x, Abs z z)]
+EAbs y (aL (EApp (EAbs z z) y)))
+
+[(x, Abs z z), (z, y)]
+EAbs y (aL z y)
+EAbs y y
+
+-}
+
+{-
+Does this terminate? Need to worry about
+
+(1) Will this always terminate?
+(2) Does it always make "progress"?
+(3) Do I need to run it multiple times
+
+1) tree never grows, leaves = {EVar, EVal, EUnit} obviously terminate.
+2) progress is defined by eliminating any lambdas in position
+
+-}
--- src/TExpr.hs
+++ src/TExpr.hs
@@ -1,42 +1,44 @@
-{-# LANGUAGE GADTs
-           , DataKinds
-           , KindSignatures
-           , RankNTypes
-           , DeriveDataTypeable
-           , AutoDeriveTypeable
-           , DeriveGeneric
-           , DeriveAnyClass
-           , TypeFamilies
-           , UndecidableInstances
-           , FlexibleContexts
-           , ScopedTypeVariables
-  #-}
+{-# LANGUAGE AutoDeriveTypeable #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module TExpr
-  ( Val(..)    
-  , TExp(..)
-  , TFunct(..)
-  , Ty(..)
-  , Rep
-  , TUnop(..)
-  , TOp(..)
-  , TVar(..)
-  , Loc
-  , TLoc(..)
-  , boolean_vars_of_texp
-  , var_of_texp
-  , loc_of_texp    
-  , te_seq
-  , last_seq
-  , exp_of_texp
-  ) where
-
-import Data.Typeable
+  ( Val (..),
+    TExp (..),
+    TFunct (..),
+    Ty (..),
+    Rep,
+    TUnop (..),
+    TOp (..),
+    TVar (..),
+    Loc,
+    TLoc (..),
+    TLambdaVar (..),
+    boolean_vars_of_texp,
+    var_of_texp,
+    loc_of_texp,
+    te_seq,
+    last_seq,
+    exp_of_texp,
+    ir_of_texp,
+  )
+where
 
 import Common
+import Data.Typeable
 import Errors
-import Field
 import Expr
+import Field
+import qualified IR
 
 data TFunct where
   TFConst :: Ty -> TFunct
@@ -44,142 +46,184 @@ data TFunct where
   TFProd :: TFunct -> TFunct -> TFunct
   TFSum :: TFunct -> TFunct -> TFunct
   TFComp :: TFunct -> TFunct -> TFunct
-  deriving Typeable
+  deriving (Typeable)
 
 data Ty where
-  TField:: Ty
+  TField :: Ty
   TBool :: Ty
-  TArr  :: Ty -> Ty
-  TProd :: Ty -> Ty -> Ty  
-  TSum  :: Ty -> Ty -> Ty
-  TMu   :: TFunct -> Ty
+  TArr :: Ty -> Ty
+  TProd :: Ty -> Ty -> Ty
+  TSum :: Ty -> Ty -> Ty
+  TMu :: TFunct -> Ty
   TUnit :: Ty
-  deriving Typeable
+  TFun :: Ty -> Ty -> Ty
+  deriving (Typeable)
 
 type family Rep (f :: TFunct) (x :: Ty) :: Ty
+
 type instance Rep ('TFConst ty) x = ty
+
 type instance Rep 'TFId x = x
+
 type instance Rep ('TFProd f g) x = 'TProd (Rep f x) (Rep g x)
+
 type instance Rep ('TFSum f g) x = 'TSum (Rep f x) (Rep g x)
+
 type instance Rep ('TFComp f g) x = Rep f (Rep g x)
 
 newtype TVar (ty :: Ty) = TVar Var
-  
-var_is_boolean :: Typeable ty => TVar ty -> Bool
-var_is_boolean x
-  = typeOf x == typeOf (undefined :: TVar 'TBool)
+
+var_is_boolean :: (Typeable ty) => TVar ty -> Bool
+var_is_boolean x =
+  typeOf x == typeOf (undefined :: TVar 'TBool)
 
 instance Eq (TVar ty) where
-  TVar x==TVar y = x==y
+  TVar x == TVar y = x == y
 
 instance Show (TVar ty) where
   show (TVar x) = show x
+
+newtype TLambdaVar (ty :: Ty) = TLambdaVar LambdaVar
 
 type Loc = Int
 
 newtype TLoc (ty :: Ty) = TLoc Loc
 
 instance Eq (TLoc ty) where
-  TLoc x==TLoc y = x==y
+  TLoc x == TLoc y = x == y
 
 instance Show (TLoc ty) where
   show (TLoc x) = "loc_" ++ show x
 
 data TUnop :: Ty -> Ty -> * where
   TUnop :: forall ty1 ty. UnOp -> TUnop ty1 ty
-  deriving Eq  
+  deriving (Eq)
 
 data TOp :: Ty -> Ty -> Ty -> * where
   TOp :: forall ty1 ty2 ty. Op -> TOp ty1 ty2 ty
-  deriving Eq  
+  deriving (Eq)
 
 data Val :: Ty -> * -> * where
-  VField :: Field a => a -> Val 'TField a
-  VTrue  :: Val 'TBool a
+  VField :: (Field a) => a -> Val 'TField a
+  VTrue :: Val 'TBool a
   VFalse :: Val 'TBool a
-  VUnit  :: Val 'TUnit a
-  VLoc   :: TLoc ty -> Val ty a
+  VUnit :: Val 'TUnit a
+  VLoc :: TLoc ty -> Val ty a
 
 data TExp :: Ty -> * -> * where
-  TEVar    :: TVar ty -> TExp ty a
-  TEVal    :: Val ty a -> TExp ty a
-  TEUnop   :: ( Typeable ty1
-              )
-             => TUnop ty1 ty -> TExp ty1 a -> TExp ty a 
-  TEBinop  :: ( Typeable ty1 
-              , Typeable ty2 
-              ) 
-             => TOp ty1 ty2 ty -> TExp ty1 a -> TExp ty2 a -> TExp ty a
-  TEIf     :: TExp 'TBool a -> TExp ty a -> TExp ty a -> TExp ty a
-  TEAssert :: Typeable ty => TExp ty a -> TExp ty a -> TExp 'TUnit a
-  TESeq    :: TExp 'TUnit a -> TExp ty2 a -> TExp ty2 a
-  TEBot    :: Typeable ty => TExp ty a
+  TEVar :: TVar ty -> TExp ty a
+  TELambdaVar :: TLambdaVar ty -> TExp ty a
+  TEVal :: Val ty a -> TExp ty a
+  TEUnop ::
+    ( Typeable ty1
+    ) =>
+    TUnop ty1 ty ->
+    TExp ty1 a ->
+    TExp ty a
+  TEBinop ::
+    ( Typeable ty1,
+      Typeable ty2
+    ) =>
+    TOp ty1 ty2 ty ->
+    TExp ty1 a ->
+    TExp ty2 a ->
+    TExp ty a
+  TEIf :: TExp 'TBool a -> TExp ty a -> TExp ty a -> TExp ty a
+  TEAssert :: (Typeable ty) => TExp ty a -> TExp ty a -> TExp 'TUnit a
+  TESeq :: TExp 'TUnit a -> TExp ty2 a -> TExp ty2 a
+  TEBot :: (Typeable ty) => TExp ty a
+  TEAbs :: (Typeable ty, Typeable ty1) => TLambdaVar ty -> TExp ty1 a -> TExp ('TFun ty ty1) a
+  TEApp :: (Typeable ty, Typeable ty1) => TExp ('TFun ty ty1) a -> TExp ty a -> TExp ty1 a
 
-exp_of_val :: Field a => Val ty a -> Exp a
+exp_of_val :: (Field a) => Val ty a -> Exp a
 exp_of_val v = case v of
   VField c -> EVal c
-  VTrue    -> EVal one
-  VFalse   -> EVal zero
-  VUnit    -> EUnit
-  VLoc l   -> fail_with $ ErrMsg $ "unresolved location " ++ show l
+  VTrue -> EVal one
+  VFalse -> EVal zero
+  VUnit -> EUnit
+  VLoc l -> fail_with $ ErrMsg $ "unresolved location " ++ show l
 
-instance ( Field a
-         , Eq a
-         )
-      => Eq (Val ty a) where
+ir_of_val :: (Field a) => Val ty a -> IR.Exp a
+ir_of_val v = case v of
+  VField c -> IR.EVal c
+  VTrue -> IR.EVal one
+  VFalse -> IR.EVal zero
+  VUnit -> IR.EUnit
+  VLoc l -> fail_with $ ErrMsg $ "unresolved location " ++ show l
+
+ir_of_texp :: (Field a) => TExp ty a -> IR.Exp a
+ir_of_texp te = case te of
+  TEVar (TVar x) -> IR.EVar x
+  TEVal v -> ir_of_val v
+  TEUnop (TUnop op) te1 ->
+    IR.EUnop op (ir_of_texp te1)
+  TEBinop (TOp op) te1 te2 ->
+    IR.exp_binop op (ir_of_texp te1) (ir_of_texp te2)
+  TEIf te1 te2 te3 ->
+    IR.EIf (ir_of_texp te1) (ir_of_texp te2) (ir_of_texp te3)
+  TEAssert te1 te2 ->
+    IR.EAssert (ir_of_texp te1) (ir_of_texp te2)
+  TESeq te1 te2 -> IR.exp_seq (ir_of_texp te1) (ir_of_texp te2)
+  TEBot -> IR.EUnit
+  TELambdaVar (TLambdaVar v) -> IR.ELambdaVar v
+  TEAbs (TLambdaVar v) e -> IR.EAbs v (ir_of_texp e)
+  TEApp e1 e2 -> IR.EApp (ir_of_texp e1) (ir_of_texp e2)
+
+instance
+  ( Field a,
+    Eq a
+  ) =>
+  Eq (Val ty a)
+  where
   v1 == v2 = exp_of_val v1 == exp_of_val v2
 
-exp_of_texp :: Field a => TExp ty a -> Exp a
-exp_of_texp te = case te of
-  TEVar (TVar x) -> EVar x
-  TEVal v -> exp_of_val v
-  TEUnop (TUnop op) te1 -> 
-    EUnop op (exp_of_texp te1)
-  TEBinop (TOp op) te1 te2 ->
-    exp_binop op (exp_of_texp te1) (exp_of_texp te2)
-  TEIf te1 te2 te3 ->
-    EIf (exp_of_texp te1) (exp_of_texp te2) (exp_of_texp te3)
-  TEAssert te1 te2 ->
-    EAssert (exp_of_texp te1) (exp_of_texp te2)
-  TESeq te1 te2 -> exp_seq (exp_of_texp te1) (exp_of_texp te2)
-  TEBot -> EUnit
+exp_of_texp :: (Field a) => TExp ty a -> Exp a
+exp_of_texp te =
+  IR.toCoreExpr $ IR.applyLambdas $ ir_of_texp te
 
-instance ( Field a
-         , Eq a
-         )
-      => Eq (TExp ty a) where
+instance
+  ( Field a,
+    Eq a
+  ) =>
+  Eq (TExp ty a)
+  where
   te1 == te2 = exp_of_texp te1 == exp_of_texp te2
 
 -- | Smart constructor for 'TESeq'.  Simplify 'TESeq te1 te2' to 'te2'
 -- whenever the normal form of 'te1' (with seq's reassociated right)
 -- is *not* equal 'TEAssert _ _'.
-te_seq :: Typeable ty1 => TExp ty1 a -> TExp ty2 a -> TExp ty2 a
-te_seq te1 te2 = case (te1,te2) of
-  (TEAssert _ _,_) -> TESeq te1 te2
-  (TESeq tx ty,_) -> te_seq tx (te_seq ty te2)
-  (_,_) -> te2
-  
-boolean_vars_of_texp :: Typeable ty => TExp ty a -> [Var]
-boolean_vars_of_texp = go []
-  where go :: Typeable ty => [Var] -> TExp ty a -> [Var]
-        go vars (TEVar t@(TVar x))
-          = if var_is_boolean t then x : vars
-            else vars
-        go vars (TEVal _) = vars
-        go vars (TEUnop _ e1) = go vars e1
-        go vars (TEBinop _ e1 e2) = go (go vars e1) e2
-        go vars (TEIf e1 e2 e3)
-          = go (go (go vars e1) e2) e3
-        go vars (TEAssert e1 e2) = go (go vars e1) e2
-        go vars (TESeq e1 e2) = go (go vars e1) e2
-        go vars TEBot = vars
+te_seq :: (Typeable ty1) => TExp ty1 a -> TExp ty2 a -> TExp ty2 a
+te_seq te1 te2 = case (te1, te2) of
+  (TEAssert _ _, _) -> TESeq te1 te2
+  (TESeq tx ty, _) -> te_seq tx (te_seq ty te2)
+  (_, _) -> te2
 
-var_of_texp :: Show a => TExp ty a -> Var
+boolean_vars_of_texp :: (Typeable ty) => TExp ty a -> [Var]
+boolean_vars_of_texp = go []
+  where
+    go :: (Typeable ty) => [Var] -> TExp ty a -> [Var]
+    go vars (TEVar t@(TVar x)) =
+      if var_is_boolean t
+        then x : vars
+        else vars
+    go vars (TEVal _) = vars
+    go vars (TEUnop _ e1) = go vars e1
+    go vars (TEBinop _ e1 e2) = go (go vars e1) e2
+    go vars (TEIf e1 e2 e3) =
+      go (go (go vars e1) e2) e3
+    go vars (TEAssert e1 e2) = go (go vars e1) e2
+    go vars (TESeq e1 e2) = go (go vars e1) e2
+    go vars TEBot = vars
+    go vars (TELambdaVar _) = vars
+    go vars (TEAbs _ e) = go vars e
+    go vars (TEApp e1 e2) = go (go vars e1) e2
+
+var_of_texp :: (Show a) => TExp ty a -> Var
 var_of_texp te = case last_seq te of
   TEVar (TVar x) -> x
   _ -> fail_with $ ErrMsg ("var_of_texp: expected var: " ++ show te)
 
-loc_of_texp :: Show a => TExp ty a -> Var
+loc_of_texp :: (Show a) => TExp ty a -> Var
 loc_of_texp te = case last_seq te of
   TEVal (VLoc (TLoc l)) -> l
   _ -> fail_with $ ErrMsg ("loc_of_texp: expected loc: " ++ show te)
@@ -195,21 +239,23 @@ instance Show (TUnop ty1 ty) where
 instance Show (TOp ty1 ty2 ty) where
   show (TOp op) = show op
 
-instance Show a => Show (Val ty a) where
+instance (Show a) => Show (Val ty a) where
   show (VField c) = show c
-  show VTrue      = "true"
-  show VFalse     = "false"  
-  show VUnit      = "()"
-  show (VLoc l)   = "loc_" ++ show l
+  show VTrue = "true"
+  show VFalse = "false"
+  show VUnit = "()"
+  show (VLoc l) = "loc_" ++ show l
 
-instance Show a => Show (TExp ty a) where
+instance (Show a) => Show (TExp ty a) where
   show (TEVar x) = "var " ++ show x
   show (TEVal c) = show c
   show (TEUnop op e1) = show op ++ show e1
   show (TEBinop op e1 e2) = show e1 ++ show op ++ show e2
-  show (TEIf b e1 e2) 
-    = "if " ++ show b ++ " then " ++ show e1 ++ " else " ++ show e2
+  show (TEIf b e1 e2) =
+    "if " ++ show b ++ " then " ++ show e1 ++ " else " ++ show e2
   show (TEAssert e1 e2) = show e1 ++ " := " ++ show e2
   show (TESeq e1 e2) = "(" ++ show e1 ++ "; " ++ show e2 ++ ")"
   show TEBot = "bot"
-
+  show (TELambdaVar (TLambdaVar v)) = show v
+  show (TEAbs (TLambdaVar v) e) = "(\\" <> show v <> " -> " <> show e <> ")"
+  show (TEApp e1 e2) = "(" <> show e1 <> ") (" <> show e2 <> ")"
