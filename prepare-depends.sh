00da4e7b26869f955d5f9e0b99826edd6f38165c
kejace <kejace@gmail.com>
2023-12-17 17:19:24 -0500

    Actually upload artifact

--- prepare-depends.sh
+++ prepare-depends.sh
@@ -14,9 +14,10 @@ cd $DEPSRC
 cd libsnark
 ./prepare-depends.sh
 make -j NO_PROCPS=1
-# make lib
-make lib STATIC=1
+make lib NO_PROCPS=1
+make lib STATIC=1 NO_PROCPS=1
 # make check
 sudo make install PREFIX=$DEPINST
+sudo cp libsnark.a /usr/lib/libsnark.a
 
 
